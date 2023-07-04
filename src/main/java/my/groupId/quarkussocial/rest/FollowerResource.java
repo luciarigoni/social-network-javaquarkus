fb53396b11fb44881dc2f52eccc4f3ceefc95557
Lucia Rigoni Freire <luciarigoni@live.com>
2023-07-04 10:36:40 -0300

    Implement follow user method

--- src/main/java/my/groupId/quarkussocial/rest/FollowerResource.java
+++ src/main/java/my/groupId/quarkussocial/rest/FollowerResource.java
@@ -1,12 +1,13 @@
 package my.groupId.quarkussocial.rest;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import my.groupId.quarkussocial.domain.model.Follower;
 import my.groupId.quarkussocial.domain.repository.FollowerRepository;
 import my.groupId.quarkussocial.domain.repository.UserRepository;
+import my.groupId.quarkussocial.rest.dto.FollowerRequest;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -19,5 +20,22 @@ public class FollowerResource {
     public FollowerResource(FollowerRepository repository, UserRepository userRepository) {
         this.repository = repository;
         this.userRepository = userRepository;
+    }
+
+    @PUT
+    public Response followUser(
+            @PathParam("userId") Long userId, FollowerRequest request) {
+        var user = userRepository.findById(userId);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        var follower = userRepository.findById(request.getFollowerId());
+        var entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+
+        repository.persist(entity);
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
---
+++ src/main/java/my/groupId/quarkussocial/rest/dto/FollowerRequest.java
@@ -0,0 +1,8 @@
+package my.groupId.quarkussocial.rest.dto;
+
+import lombok.Data;
+
+@Data
+public class FollowerRequest {
+    private Long followerId;
+}
