1b7e3983226450c085adbb3cbd1815c57051f908
Lucia Rigoni Freire <luciarigoni@live.com>
2023-07-05 13:59:44 -0300

    Create and test PostResources

--- src/test/java/my/groupId/quarkussocial/rest/PostResourcesTest.java
+++ src/test/java/my/groupId/quarkussocial/rest/PostResourcesTest.java
@@ -1,7 +1,126 @@
 package my.groupId.quarkussocial.rest;
 
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import my.groupId.quarkussocial.domain.model.UserEntity;
+import my.groupId.quarkussocial.domain.repository.UserRepository;
+import my.groupId.quarkussocial.rest.dto.CreatePostRequest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
+@QuarkusTest
+@TestHTTPEndpoint(PostResources.class)
 class PostResourcesTest {
 
+    @Inject
+    UserRepository userRepository;
+    Long userId;
+
+    @BeforeEach
+    @Transactional
+    public void setUP() {
+        var user = new UserEntity();
+        user.setAge(30);
+        user.setName("Fulano");
+        userRepository.persist(user);
+        userId = user.getId();
+    }
+    @Test
+    @DisplayName("should create a post for a user")
+    public void createPostTest(){
+        var postRequest = new CreatePostRequest();
+        postRequest.setText("Some text");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(postRequest)
+                .pathParam("userId", userId)
+                .when()
+                .post()
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    @DisplayName("should return 404 when trying to make a post for an inexistent user")
+    public void postForAnInexistentUserTest(){
+        var postRequest = new CreatePostRequest();
+        postRequest.setText("Some text");
+
+        var inexistentUserId = 999;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(postRequest)
+                .pathParam("userId", inexistentUserId)
+                .when()
+                .post()
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("should return 404 when user does not exist")
+    public void listPostUserNotFoundText() {
+
+        var inexistentUserId = 999;
+        given()
+                .pathParam("userId", inexistentUserId)
+                .when()
+                .get()
+                .then()
+                .statusCode(404);
+
+    }
+
+    @Test
+    @DisplayName("should return 404 when followerId header is not present")
+    public void listPostFollowerHeaderNotSentText() {
+
+        given()
+                .pathParam("userId", userId)
+                .when()
+                .get()
+                .then()
+                .statusCode(400)
+                .body(Matchers.is("You forgot the header followerId"));
+
+    }
+
+    @Test
+    @DisplayName("should return 404 when follower does not exist")
+    public void listFollowerNotFoundText() {
+
+        var inexistentFollowerId = 999;
+
+        given()
+                .pathParam("userId", userId)
+                .header("followerId", inexistentFollowerId)
+                .when()
+                .get()
+                .then()
+                .statusCode(400)
+                .body(Matchers.is("Inexistent followerId"));
+
+    }
+
+    @Test
+    @DisplayName("should return 203 when follower is not follower")
+    public void listPostNotAFollower() {
+
+    }
+
+//    @Test
+//    @DisplayName("should return posts")
+//    public void listPostNotAFollower() {
+//
+//    }
 }
